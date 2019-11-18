8b2f7162b3abcca844e295242eba67e5594c6f7e
rajkumar-101 <rajkumar.suresh@cognizant.com>
2019-11-18 17:28:00 +0530

    start screen added with button
    Start screen has been created with a button to transition to main scene

---
+++ innovators-ar/Assets/loader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loader : MonoBehaviour
+{
+    public Transform button_action;
+
+    private void Awake()
+    {
+        button_action.GetComponent<Button>().onClick.AddListener(delegate { SceneManager.LoadScene("SampleScene"); });
+
+    }
+}
